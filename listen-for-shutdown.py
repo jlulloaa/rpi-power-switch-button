1a45da82f4e7a832194d06b02ec878621caaecfd
Jose L. Ulloa <84105116+jlulloaa@users.noreply.github.com>
2024-08-11 22:32:12 -0400

    fix: corrected indentation error in executable

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-    """_summary_
-        This file listens for a change in the switch button (i.e. when it's pressed)
-        This file has to be manually copied into /usr/local/bin so the associated service file (listen-for-shutdown.service) can find it
-        
-    """
+"""_summary_
+    This file listens for a change in the switch button (i.e. when it's pressed)
+    This file has to be manually copied into /usr/local/bin so the associated service file (listen-for-shutdown.service) can find it
+    
+"""
 import RPi.GPIO as GPIO
 import subprocess
 
